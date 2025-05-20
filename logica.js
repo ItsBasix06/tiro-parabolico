12ea7d9e86a0766e48f0a594ca8e40e7bbfa7fa7
ItsBasix06 <basiliocordobaarcas132@gmail.com>
2025-05-20 21:39:47 +0200

    hago la funcion para pasar a radianes los grados #3

--- logica.js
+++ logica.js
@@ -2,3 +2,8 @@ function calculaDistancia( angulo, vel-inc ) {
   let distancia = vel-ini * vel-ini * Math.sin(2*angulo)/9.81;
     return distancia;
 }
+
+function gradosARadianes(grados) {
+  let radianes =  grados * (Math.PI / 180);
+  return radianes;
+}
